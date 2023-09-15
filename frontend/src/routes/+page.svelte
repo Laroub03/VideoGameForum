f16d4a3238185de4e6fc3843c4e561f3285a5e6c
emil <emil@emil>
2023-09-15 12:13:49 +0200

    Added frontend.

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let email = '';
+	let password = '';
+</script>
+
+<input class="input" type="text" name="email" bind:value={email} placeholder="e-mail" />
+<input class="input" type="password" name="password" bind:value={password} placeholder="password" />
